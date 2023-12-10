92fac0bad2d57319e79e720944b8b80508f3d647
dsobieraj <dsobieraj@outlook.com>
2023-12-10 19:01:25 +0100

    Create app layout

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import random
 import csv
 from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QColor
 from PySide6.QtWidgets import QSlider
 
 
@@ -21,6 +22,7 @@ class App(QtWidgets.QWidget):
         # False - simple, True - weighting inversed squared distance
         self.voteType = False
 
+        self.sliderLabel = QtWidgets.QLabel("Liczba sąsiadów")
         self.slider = QtWidgets.QSlider(Qt.Orientation.Horizontal)
         self.slider.setTickPosition(QSlider.TickPosition.TicksBelow)
         self.slider.setTickInterval(1)
@@ -28,8 +30,29 @@ class App(QtWidgets.QWidget):
         self.slider.setValue(self.neighbourCount)
         self.slider.valueChanged.connect(self.update_slider)
 
+        self.metricLabel = QtWidgets.QLabel("Rodzaj metryki")
+        self.metricButton = QtWidgets.QPushButton(("Euklidesowa", "Miejska")[self.metricType])
+        self.metricButton.clicked.connect(self.update_metric)
+
+        self.voteLabel = QtWidgets.QLabel("Rodzaj głosowania")
+        self.voteButton = QtWidgets.QPushButton(("Prosty", "Ważony")[self.voteType])
+        self.voteButton.clicked.connect(self.update_type)
+
+        self.innerLayout = QtWidgets.QVBoxLayout()
+        self.innerLayout.addWidget(self.sliderLabel)
+        self.innerLayout.addWidget(self.slider)
+        self.innerLayout.addWidget(self.metricLabel)
+        self.innerLayout.addWidget(self.metricButton)
+        self.innerLayout.addWidget(self.voteLabel)
+        self.innerLayout.addWidget(self.voteButton)
+
         self.layout = QtWidgets.QHBoxLayout(self)
-        self.layout.addWidget(self.slider)
+        self.pixmap = QtGui.QPixmap(600, 600)
+        self.pixmap.fill(QColor(0, 0, 0))
+        self.pixmapLabel = QtWidgets.QLabel()
+        self.pixmapLabel.setPixmap(self.pixmap)
+        self.layout.addWidget(self.pixmapLabel)
+        self.layout.addLayout(self.innerLayout)
 
         # Read the input file and store rows in rows var
         file = open("input.txt")
@@ -37,10 +60,22 @@ class App(QtWidgets.QWidget):
         rows = []
         for row in csvreader:
             rows.append(row)
-
         file.close()
 
     @QtCore.Slot()
     def update_slider(self, value):
         print(value)
 
+    @QtCore.Slot()
+    def update_metric(self):
+        self.metricType = not self.metricType
+        self.metricButton.setText(("Euklidesowa", "Miejska")[self.metricType])
+
+    @QtCore.Slot()
+    def update_type(self):
+        self.voteType = not self.voteType
+        self.voteButton.setText(("Prosty", "Ważony")[self.voteType])
+
+    def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
+        if event.button() == Qt.LeftButton:
+            print(event.pos())
