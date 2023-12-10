df3cf240aa83b287dde6f41d6bfa1ebd33744697
dsobieraj <dsobieraj@outlook.com>
2023-12-10 15:13:28 +0100

    git ignore update

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import PySide6.QtCore
+
+# Prints PySide6 version
+print(PySide6.__version__)
+
+# Prints the Qt version used to compile PySide6
+print(PySide6.QtCore.__version__)
